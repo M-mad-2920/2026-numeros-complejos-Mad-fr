2db5760a3e3cc4ddfcc6b90d04708e2dc8252008
Matías  Mad <matutemad2009@gmail.com>
2026-04-24 10:29:14 -0300

    Códiguirijillo de Kotlin-Mad

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,20 @@
+package org.example
+
+class Complejo {
+    private var real: Int=0
+    private var imaginario: Int=0
+    fun inicializar(real: Int, imaginario:Int){
+        this.real=real
+        this.imaginario=imaginario
+    }
+    override fun toString(): String {
+        return "(${real},${imaginario})"
+    }
+}
+
+fun main() {
+    var complejo: Complejo
+    complejo=Complejo()
+    complejo.inicializar(3,4)
+    println("El número complejo es: ${complejo.toString()}")
+}
